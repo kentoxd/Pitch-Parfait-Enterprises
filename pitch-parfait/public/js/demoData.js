33dcca9de85ea2477cf2c52c872f7328db969ca3
Owen Campued <qofcampued@tip.edu.ph>
2026-04-10 20:22:35 +0800

    unrefined categories

--- pitch-parfait/public/js/demoData.js
+++ pitch-parfait/public/js/demoData.js
@@ -1,67 +1,211 @@
 export const demoProducts = [
   {
-    id: "pf-strawberry-cloud",
-    name: "Strawberry Cloud Parfait",
-    category: "Parfait",
-    price: 159,
-    image: "https://cdn.discordapp.com/attachments/1488093221014994986/1488124196893626519/Berry_Parfait.png?ex=69cba328&is=69ca51a8&hm=f8b7c2a9ec91749c3972a940942c61da387ba8734bbe63a29868857da829fc6b&",
-    description: "Layered strawberries, vanilla cream, and crunchy crumble."
-  },
-  {
     id: "pf-mango-sunset",
     name: "Mango Sunset Parfait",
     category: "Parfait",
     price: 149,
-    image: "https://cdn.discordapp.com/attachments/1488093221014994986/1488124248835883028/Mango_Strawberry_Parfait.png?ex=69cba334&is=69ca51b4&hm=a779de828810a027ed5a9730cfa86e149efeb0f0ac98605642033be8f1727c16&",
+    image: "../assets/parfait/Mango Strawberry Parfait.png",
     description: "Bright mango, coconut cream, and toasted flakes."
   },
   {
-    id: "sw-berry-silk",
-    name: "Berry Silk Swirl",
+    id: "pf-berry",
+    name: "Berry Parfait",
+    category: "Parfait",
+    price: 149,
+    image: "../assets/parfait/Berry Parfait.png",
+    description: "Bright mango, coconut cream, and toasted flakes."
+  },
+  {
+    id: "pf-nutty-choco-candy",
+    name: "Nutty Chocolate Candy Parfait",
+    category: "Parfait",
+    price: 149,
+    image: "../assets/parfait/Nutty-Chocolate-Candy Parfait.png",
+    description: "Bright mango, coconut cream, and toasted flakes."
+  },
+  {
+    id: "pf-oreo-mudpie",
+    name: "Oreo Mudpie Parfait",
+    category: "Parfait",
+    price: 149,
+    image: "../assets/parfait/Oreo-Mudpie Parfait.png",
+    description: "Bright mango, coconut cream, and toasted flakes."
+  },
+  {
+    id: "pf-purple-dream",
+    name: "Purple Dream Parfait",
+    category: "Parfait",
+    price: 149,
+    image: "../assets/parfait/Purple-Dream Parfait.png",
+    description: "Bright mango, coconut cream, and toasted flakes."
+  },
+  {
+    id: "sw-berry-choco",
+    name: "Berry Choco Swirl",
     category: "Swirls",
     price: 129,
-    image: "https://placehold.co/900x700/png?text=Berry+Silk+Swirl",
+    image: "../assets/swirls/berry-choco.png",
     description: "Frozen yogurt swirl with mixed berry ribbon."
   },
   {
-    id: "sw-choco-rose",
-    name: "Choco Rose Swirl",
+    id: "sw-choco-cookie-dough",
+    name: "Chocolate Chip Dough Swirl",
     category: "Swirls",
-    price: 139,
-    image: "https://placehold.co/900x700/png?text=Choco+Rose+Swirl",
-    description: "Chocolate swirl with a hint of rose and crunch."
+    price: 129,
+    image: "../assets/swirls/chocolate-chip-cookie-dough-swirls.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
   },
   {
-    id: "sh-vanilla-dream",
-    name: "Vanilla Dream Shake",
+    id: "sw-french-silk-pie",
+    name: "French Silk Pie Swirl",
+    category: "Swirls",
+    price: 129,
+    image: "../assets/swirls/french-silk-pie.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
+  },
+  {
+    id: "sw-mint-oreo",
+    name: "Mint Oreo Swirl",
+    category: "Swirls",
+    price: 129,
+    image: "../assets/swirls/Mint-Oreo-Swirls.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
+  },
+  {
+    id: "sw-royal-ny-cheesecake",
+    name: "Royal New York Cheesecake Swirl",
+    category: "Swirls",
+    price: 129,
+    image: "../assets/swirls/Royal-New-York-Cheesecake-Swirls.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
+  },
+  {
+    id: "sw-royal-oreo",
+    name: "Royal Oreo Swirl",
+    category: "Swirls",
+    price: 129,
+    image: "../assets/swirls/Royal-Oreo-Swirls.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
+  },
+  {
+    id: "sw-royal-reeses-brownie",
+    name: "Royal Reese's Brownie Swirl",
+    category: "Swirls",
+    price: 129,
+    image: "../assets/swirls/Royal-Reese's-Brownie-Swirls.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
+  },
+  {
+    id: "sw-royal-ultimat-choco-brownie",
+    name: "Royal Ultimate Choco Brownie Swirl",
+    category: "Swirls",
+    price: 129,
+    image: "../assets/swirls/Royal-Ultimate-Choco-Brownie-Swirls.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
+  },
+  {
+    id: "sw-turtle-pecan",
+    name: "Turtle Pecan Swirl",
+    category: "Swirls",
+    price: 129,
+    image: "../assets/swirls/Turtle-Pecan-Swirls.png",
+    description: "Frozen yogurt swirl with mixed berry ribbon."
+  },
+  {
+    id: "sh-caramel-moolatte",
+    name: "Caramel Moolatte Shake",
     category: "Shakes",
     price: 169,
-    image: "https://placehold.co/900x700/png?text=Vanilla+Dream+Shake",
+    image: "../assets/milks/caramel-moolatte-milkshake.png",
     description: "Classic vanilla shake topped with whipped cream."
   },
   {
-    id: "sh-cookie-crush",
-    name: "Cookie Crush Shake",
+    id: "sh-frozen-choco",
+    name: "Frozen Hot Chocolate Shake",
     category: "Shakes",
-    price: 179,
-    image: "https://placehold.co/900x700/png?text=Cookie+Crush+Shake",
-    description: "Cookies, cream, and a drizzle of fudge."
+    price: 169,
+    image: "../assets/milks/frozen_hot_chocolate_milkshake.png",
+    description: "Classic vanilla shake topped with whipped cream."
   },
   {
-    id: "ck-red-velvet",
-    name: "Mini Red Velvet Cake",
+    id: "sh-hot-fudge",
+    name: "Hot Fudge Shake",
+    category: "Shakes",
+    price: 169,
+    image: "../assets/milks/hot-fudge-milkshake.png",
+    description: "Classic vanilla shake topped with whipped cream."
+  },
+  {
+    id: "sh-mocha-moolatte",
+    name: "Mocha Moolatte Shake",
+    category: "Shakes",
+    price: 169,
+    image: "../assets/milks/mocha-moolatte-milkshake.png",
+    description: "Classic vanilla shake topped with whipped cream."
+  },
+  {
+    id: "sh-strawberry",
+    name: "Strawberry Shake",
+    category: "Shakes",
+    price: 169,
+    image: "../assets/milks/strawberry-milkshake.png",
+    description: "Classic vanilla shake topped with whipped cream."
+  },
+  {
+    id: "ck-choco-ex",
+    name: "Chocolate Extreme Cake",
     category: "Cakes",
     price: 199,
-    image: "https://placehold.co/900x700/png?text=Red+Velvet+Cake",
+    image: "../assets/cake/DQ-Blizzard-Ice-Cream-Cake-Chocolate-Extreme.png",
     description: "Soft red velvet with silky cream cheese frosting."
   },
   {
-    id: "ck-strawberry-short",
-    name: "Strawberry Shortcake",
+    id: "ck-choco-kitkat",
+    name: "Chocolate Kitkat Cake",
     category: "Cakes",
-    price: 219,
-    image: "https://placehold.co/900x700/png?text=Strawberry+Shortcake",
-    description: "Light sponge, strawberries, and whipped cream."
-  }
+    price: 199,
+    image: "../assets/cake/DQ-Blizzard-Ice-Cream-Cake-Chocolate-Kitkat.png",
+    description: "Soft red velvet with silky cream cheese frosting."
+  },
+  {
+    id: "ck-choco-shavings",
+    name: "Choco Shavings Cake",
+    category: "Cakes",
+    price: 199,
+    image: "../assets/cake/DQ-Ice-Cream-Choco-Shavings.png",
+    description: "Soft red velvet with silky cream cheese frosting."
+  },
+  {
+    id: "ck-purple-fudge-cheesecake",
+    name: "Purple Fudge Cheesecake",
+    category: "Cakes",
+    price: 199,
+    image: "../assets/cake/WEB_Purple-Fudge-Cheesecake-8-04-1024x1024.png",
+    description: "Soft red velvet with silky cream cheese frosting."
+  },
+  {
+    id: "ck-choco-oreo",
+    name: "Choco Oreo Cake",
+    category: "Cakes",
+    price: 199,
+    image: "../assets/tincake/Tin-Cakes-Choco-Oreo.png",
+    description: "Soft red velvet with silky cream cheese frosting."
+  },
+  {
+    id: "ck-mango-cheesecake",
+    name: "Mango Cheesecake",
+    category: "Cakes",
+    price: 199,
+    image: "../assets/tincake/Tin-Cakes-Mango-Cheesecake.png",
+    description: "Soft red velvet with silky cream cheese frosting."
+  },
+  {
+    id: "ck-strawberry-cashew-cheesecake",
+    name: "Strawberry Cashew Cheesecake",
+    category: "Cakes",
+    price: 199,
+    image: "../assets/tincake/Tin-Cakes-Strawberry-Cashew-Cheesecake.png",
+    description: "Soft red velvet with silky cream cheese frosting."
+  },
 ];
 
